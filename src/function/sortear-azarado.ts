0c18b873c20ada124a7301011354397f08e2fb41
lucas.requiao <lucas.requiao@pm.df.gov.br>
2026-04-23 09:35:50 -0300

    adicionado suporte para sorteio de um azarado com progresso visual e refatoração do código

--- src/function/sortear-azarado.ts
+++ src/function/sortear-azarado.ts
@@ -1,10 +1,15 @@
 import Pessoa from "../model/pessoa";
+import Progresso from "../ui/progresso";
 
-export default function sortearAzarado(pessoas: Pessoa[]): Pessoa {
+export default async function sortearAzarado(pessoas: Pessoa[], duracaoEmSegundos: number): Promise<Pessoa> {
     let restantes = [...pessoas]
+    const progresso = new Progresso(pessoas.length)
+    const tempoEspera = (duracaoEmSegundos / pessoas.length) * 1000
 
     for (let i = 0; i < pessoas.length; i++) {
         restantes = todosMenosUm(restantes)
+        await esperar(tempoEspera)
+        progresso.atualizar(i+1)
     }
     return restantes[0]
 }
@@ -16,4 +21,10 @@ function todosMenosUm(pessoas: Pessoa[]): Pessoa[] {
     const pessoaRemovida = pessoas.splice(sorteado, 1)
     //console.log(`A pessoa removida foi: ${pessoaRemovida.toString()}`)
     return pessoas
+}
+
+function esperar(ms: number): Promise<void>{
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), ms)
+    })
 }
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import carregarPessoas from "./function/carregar-pessoas";
 import sortearAzarado from "./function/sortear-azarado";
 import terminal from "./ui/terminal";
 
+async function rodar(){
+    const pessoas = carregarPessoas("src/data/dados.csv")
+    const duracaoEmSegundos = 10
+    const azarado = await sortearAzarado(pessoas, duracaoEmSegundos)
 
-const pessoas = carregarPessoas("src/data/dados.csv")
+    terminal.clear()
 
-const azarado = sortearAzarado(pessoas)
+    terminal.white("\n\nO ").brightRed("AZARADO")
+    terminal.bold.brightGreen(" MAIS SORTUDO").white(" é: \n")
+    terminal.bold.brightYellow(azarado.toString())
+    terminal("\n\n🎉🎉🎉🥳🥳🥳🎉🎉🎉🥳🥳🥳")
+    console.log()
+}
 
-terminal.clear()
-
-terminal.white("\n\nO ").brightRed("AZARADO")
-terminal.bold.brightGreen(" MAIS SORTUDO").white(" é: \n")
-terminal.bold.brightYellow(azarado.toString())
-terminal("\n\n🎉🎉🎉🥳🥳🥳🎉🎉🎉🥳🥳🥳")
-console.log()
+rodar()
